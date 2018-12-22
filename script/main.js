6ad2246cae52da99f4cfbb714d9ca4803ee4cd33
Leonardo <leonardoorozco69@gmail.com>
2018-12-22 06:27:53 -0600

    Scroll-to-top

--- script/main.js
+++ script/main.js
@@ -8,3 +8,19 @@ function hideMenu()
 {
     document.getElementById("menu").style.left = "-100vw";
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("up").style.display = "block";
+  } else {
+    document.getElementById("up").style.display = "none";
+  }
+}
+
+
+function subir() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
